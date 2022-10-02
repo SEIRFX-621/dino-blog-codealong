8ed0981ab517f3d8c6b379d525e4c2a1465dc133
jtg91389 <jgear91389@gmail.com>
2022-10-01 20:04:43 -0500

    Remake comment component as class

--- src/Comment.js
+++ src/Comment.js
@@ -1,19 +1,29 @@
-import React, { useState} from 'react';
+import React from 'react';
 
-function Comment({body}) {
-  const [bodyValue, setBodyValue] = useState(body);
-  return (
-    <div className="comment">
-      <p>{bodyValue}</p>
-      <form onSubmit={(e) => {
-        e.preventDefault();
-        setBodyValue(e.target.body.value);
-      }}>
-        <input name="body" type="text" placeholder={bodyValue} />
-        <button type="submit">Submit comment change</button>
-      </form>
-    </div>
-  );
+class Comment extends React.Component {
+  constructor({body}) {
+    super(body);
+    this.state = {
+      value: body
+    };
+  }
+
+  render () {
+    return (
+      <div className="comment">
+        <p>{this.state.value}</p>
+        <form onSubmit={(e) => {
+          e.preventDefault();
+          this.setState({
+            value: e.target.body.value
+          });
+        }}>
+          <input name="body" type="text" placeholder={this.state.value} />
+          <button type="submit">Submit comment change</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Comment;
